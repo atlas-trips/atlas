3d6d45726b41454534f7b65bc3477920045eeb3e
Devam M <devam493@gmail.com>
2018-09-29 18:56:11 -0400

    basic landing page

---
+++ client/components/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Landing = () => {
+    return (
+        <div className="landing-container" >
+           <div className="landing-image">
+            <img src='images/splash.jpg' width="100%" />
+            </div>
+            <div className="landing-text">
+                <h1>What was the slogan again</h1>
+                <button>sign up</button>
+            </div>
+        </div>
+    )
+}
+
+
+module.exports = Landing; 
