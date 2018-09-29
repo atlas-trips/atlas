1b1a28481966f5e7b7e4963e33061b13c5094e3e
Devam M <devam493@gmail.com>
2018-09-29 19:27:19 -0400

    added links

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -7,9 +7,12 @@ const Landing = () => {
            <div className="landing-image">
             <img src='images/splash.jpg' width="100%" />
             </div>
+            <div className="landing-header">
+                <a href="login"><h4>Login</h4></a>
+            </div>
             <div className="landing-text">
                 <h1>What was the slogan again</h1>
-                <button>sign up</button>
+                <a href="/signup"><button>sign up</button></a>
             </div>
         </div>
     )
