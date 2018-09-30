d4b498a9de99d3d21e67a5531e33d88a27a66df7
mbungeroth <mbungeroth@gmail.com>
2018-09-30 18:25:23 -0400

    #34 add more to seed file

--- client/components/SingleTrip.js
+++ client/components/SingleTrip.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ParticipantsOverview from './ParticipantsOverview'
 import ActivitiesOverview from './ActivitiesOverview'
-import AccommodationsOverview from './AccommodationsOverview'
 
 const divStyle = {
   textAlign: 'center',
