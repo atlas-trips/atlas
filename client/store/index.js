b7eadfd89039dc4509cee898f3f4ead2c8904417
Devam M <devam493@gmail.com>
2018-10-01 14:55:37 -0400

    created route to get trips and trip reducer

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import accommodation from './accommodation';
+import trip from './trip';
 
-const reducer = combineReducers({user, accommodation})
+const reducer = combineReducers({user, accommodation, trip})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -14,3 +15,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './accommodation'
+export * from './trip'
---
+++ client/store/trip.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const GET_TRIPS = 'GET_TRIPS';
+const GET_SELECTED_TRIP = 'GET_SELECTED_TRIP';
+
+const defaultTrip = {
+    all: [],
+    selected: {}
+}
+
+const getTrips = trips => ({type: GET_TRIPS, trips});
+const getSelected = trip => ({type: GET_SELECTED_TRIP, trip});
+
+export const fetchTrips = (id) => async dispatch => {
+    try{
+        const res = await axios.get(`/api/users/${id}/trips`);
+        dispatch(getTrips(res.data));
+        //sets first trip as selected for now, will update  
+        dispatch(getSelected(res.data[0]));
+    } catch (err){
+        console.log(err);
+    }
+}
+
+export default function (state = defaultTrip, action){
+    switch(action.type){
+        case GET_TRIPS:
+            return {...state, all: action.trips}
+        case GET_SELECTED_TRIP:
+              
+            return {...state, selected: action.trip}
+        default:
+            return state;
+    }
+}
