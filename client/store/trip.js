ec6db94e52097bf432860fed9aae7f5299ea7622
Kemet Dugue <kemet.dugue@gmail.com>
2018-10-02 15:20:19 -0400

    change link on sidebar itinerary to activities

--- client/store/trip.js
+++ client/store/trip.js
@@ -1,41 +1,41 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const GET_TRIPS = 'GET_TRIPS';
-const GET_SELECTED_TRIP = 'GET_SELECTED_TRIP';
+const GET_TRIPS = 'GET_TRIPS'
+const GET_SELECTED_TRIP = 'GET_SELECTED_TRIP'
 
 const defaultTrip = {
-    all: [],
-    selected: {}
+  all: [],
+  selected: {}
 }
 
-const getTrips = trips => ({type: GET_TRIPS, trips});
-const getSelected = trip => ({type: GET_SELECTED_TRIP, trip});
+const getTrips = trips => ({type: GET_TRIPS, trips})
+const getSelected = trip => ({type: GET_SELECTED_TRIP, trip})
 
-export const fetchTrips = (id) => async dispatch => {
-    try{
-        const res = await axios.get(`/api/users/${id}/trips`);
-        dispatch(getTrips(res.data));
-    } catch (err){
-        console.log(err);
-    }
+export const fetchTrips = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/users/${id}/trips`)
+    dispatch(getTrips(res.data))
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export const fetchSelected = (tripId) => async dispatch => {
-    try {
-        const trip = await axios.get(`/api/trips/${tripId}`);
-        dispatch(getSelected(trip.data))
-    } catch (err){
-        console.log(err);
-    }
+export const fetchSelected = tripId => async dispatch => {
+  try {
+    const trip = await axios.get(`/api/trips/${tripId}`)
+    dispatch(getSelected(trip.data))
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-export default function (state = defaultTrip, action){
-    switch(action.type){
-        case GET_TRIPS:
-            return {...state, all: action.trips}
-        case GET_SELECTED_TRIP:
-            return {...state, selected: action.trip}
-        default:
-            return state;
-    }
+export default function(state = defaultTrip, action) {
+  switch (action.type) {
+    case GET_TRIPS:
+      return {...state, all: action.trips}
+    case GET_SELECTED_TRIP:
+      return {...state, selected: action.trip}
+    default:
+      return state
+  }
 }
