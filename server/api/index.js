9e32fb84352c7102ac260b0e2c7cc584ecace876
Jim Yang <jyang_123@hotmail.com>
2018-10-02 20:04:59 -0400

    travel can fetch data and display form

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,13 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+module.exports = router;
 
-router.use('/users', require('./users'))
-router.use('/accommodations', require('./accommodations'))
-router.use('/trips', require('./trips'))
+router.use('/users', require('./users'));
+router.use('/accommodations', require('./accommodations'));
+router.use('/trips', require('./trips'));
+router.use('/transportation', require('./transportation'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
