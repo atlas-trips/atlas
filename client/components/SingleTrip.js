867a132f0454d2ce3948a9a254722b3da3671a33
Jim Yang <jyang_123@hotmail.com>
2018-10-09 13:33:06 -0400

    background image for single trip view

--- client/components/SingleTrip.js
+++ client/components/SingleTrip.js
@@ -42,35 +42,51 @@ class SingleTrip extends Component {
 
   render() {
     const trip = this.props.trip;
+    console.log('trip', trip);
+    const singleTripStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundImage: `url('/images/${trip.name.toLowerCase()}.jpg')`
+    };
 
     if (Object.keys(this.props.trip).length) {
       return (
         <div className="single-trip">
           <Sidebar />
-          <div className="single-trip-header">
-            {trip.name}: {trip.startDate.slice(0, 10)} to{' '}
-            {trip.endDate.slice(0, 10)}
-            <button
-              style={{...divStyle, color: 'black'}}
-              onClick={this.handleClick}
-            >
-              Invite your Friends {this.state.open ? <ShareTrip /> : null}
-            </button>
-            <button
-              style={{...divStyle, color: 'black'}}
-              onClick={this.handleDelete}
-            >
-              Remove Trip
-            </button>
-          </div>
-          <div className="single-trip-info">
-            <div className="single-trip-info-top">
-              <ParticipantsOverview peeps={trip.users} />
+          <div className="single-trip-right" style={singleTripStyle}>
+            <div className="single-trip-header">
+              <div className="single-trip-header-name">{trip.name}:</div>
+              <div className="single-trip-header-dates">
+                <span>
+                  {trip.startDate.slice(0, 10)} to {trip.endDate.slice(0, 10)}
+                </span>
+              </div>
+              <div className="single-trip-header-buttons">
+                <button
+                  className="single-trip-header-buttons-invite"
+                  style={{...divStyle, color: 'black'}}
+                  onClick={this.handleClick}
+                >
+                  INVITE YOUR FRIENDS! {this.state.open ? <ShareTrip /> : null}
+                </button>
+                <button
+                  className="single-trip-header-buttons-remove"
+                  style={{...divStyle, color: 'black'}}
+                  onClick={this.handleDelete}
+                >
+                  REMOVE TRIP
+                </button>
+              </div>
             </div>
+            <div className="single-trip-info">
+              <div className="single-trip-info-top">
+                <ParticipantsOverview peeps={trip.users} />
+              </div>
 
-            <div className="single-trip-info-bottom">
-              <ActivitiesOverview activities={trip.activities} />
-              <AccommodationOverview accommodations={trip.accommodation} />
+              <div className="single-trip-info-bottom">
+                <ActivitiesOverview activities={trip.activities} />
+                <AccommodationOverview accommodations={trip.accommodation} />
+              </div>
             </div>
           </div>
         </div>
