b5f02972c68aadd99f42c763229003955e47b389
mbungeroth <mbungeroth@gmail.com>
2018-10-10 13:08:16 -0400

    add basic styling for situation where no itinerary

--- server/api/accommodations.js
+++ server/api/accommodations.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const {Accommodation, User} = require('../db/models');
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
-  try {
-    const accomodations = await Accommodation.findAll();
-    res.json(accomodations);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const accomodations = await Accommodation.findAll();
+//     res.json(accomodations);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 router.post('/', async (req, res, next) => {
   try {
