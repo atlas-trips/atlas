2e1b9aa9324dd2a8cbd7ec3918fa989685e05d4d
Devam M <devam493@gmail.com>
2018-10-12 11:10:53 -0400

    key fix

--- server/api/index.js
+++ server/api/index.js
@@ -11,7 +11,7 @@ router.use('/transportation', require('./transportation'));
 router.get('/:id', async(req, res, next) => {
   console.log('CID YO)))))))))))))')
   try{
-    const {data: place} = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?cid=${req.params.id}&key=AIzaSyD4jSOU0XG9zooC14hIs9G`)
+    const {data: place} = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?cid=${req.params.id}&key=AIzaSyD4jSOU0XG9zooC14hIs9G_zTNkEQ6zd_g`)
     console.log('RESULT ',place)
     res.json(place)
     
